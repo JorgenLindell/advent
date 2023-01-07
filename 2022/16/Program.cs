9c069fa2db7c37fbd5e0dbb86e0d123a1f4f8222
JorgenLindell <jorgen.lindell@tetrapak.com>
2023-01-07 01:57:38 +0100

    Speed up 16 a bil by using a bitmap

--- 2022/16/Program.cs
+++ 2022/16/Program.cs
@@ -20,104 +20,96 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II"
             .Replace("\r\n", "\n");
 
+    private static bool _debug = false;
     private static void Main(string[] args)
     {
-        var debug = false;
-        FirstPart(GetDataStream(debug), debug);
-        SecondPart(GetDataStream(debug), debug);
+        FirstPart(GetDataStream );
+        SecondPart(GetDataStream );
     }
 
-    private static TextReader GetDataStream(bool debug) =>
-        debug
+    private static TextReader GetDataStream() =>
+        _debug
             ? StreamUtils.GetInputStream(testData: _testData)
             : StreamUtils.GetInputStream("input.txt");
 
-    private static void SecondPart(TextReader stream, bool debug)
+    private static void SecondPart(Func<TextReader> stream)
     {
         Dictionary<string, Node> nodes;
-        nodes = LoadFile(stream, out string start);
+        nodes = LoadFile(stream());
+
         var startNode = nodes["AA"];
         var cache = new Dictionary<string, int>();
         var productiveNodesCount = nodes.Values.Count(x => x.Flow > 0);
 
-        var result = MaxFlow(startNode, new HashSet<string>(), "", 26, false);
+        var result = MaxFlow(startNode, 0ul, 26, false);
         Debug.WriteLine("result=" + result);
 
 
 
-        int MaxFlow(Node current, HashSet<string> opened, string openedText, int timeLeft, bool elephant)
+        int MaxFlow(Node current, ulong openedBm, int timeLeft, bool elephant)
         {
-            if (opened.Count == productiveNodesCount)
+            if (BitCounter.CountSetBits(openedBm) == productiveNodesCount)
                 return 0;
 
             if (timeLeft <= 0)
             {
                 if (!elephant)
                 {
-                    return MaxFlow(startNode, opened, openedText, 26, true);
+                    return MaxFlow(startNode, openedBm, 26, true);
                 }
                 return 0;
             }
 
-
-            var key = $"{current.Name};{openedText};{((char)timeLeft)};{elephant}";
+            var key = $"{current.Name};{openedBm};{((char)timeLeft)};{elephant}";
             if (cache.ContainsKey(key))
                 return cache[key];
 
-
             var maxFlow = 0;
-            if (current.Flow > 0 && !opened.Contains(current.Name))
+            if (current.Flow > 0 && ((openedBm & (1u << current.Number)) == 0))
             {
-                var currentOpened = opened.Append(current.Name).ToHashSet();
-                var currentOpenedText = string.Join(',', currentOpened.OrderBy(x=>x));
-                maxFlow = (current.Flow * (timeLeft - 1)) + MaxFlow(current, currentOpened, currentOpenedText, timeLeft - 1, elephant);
+                maxFlow = (current.Flow * (timeLeft - 1)) + MaxFlow(current, openedBm | 1u << current.Number, timeLeft - 1, elephant);
             }
             foreach (var edge in current.Edges.Values)
             {
-                maxFlow = Math.Max(maxFlow, MaxFlow(edge.To, opened, openedText, timeLeft - 1, elephant));
+                maxFlow = Math.Max(maxFlow, MaxFlow(edge.To, openedBm, timeLeft - 1, elephant));
             }
 
             cache[key] = maxFlow;
             return maxFlow;
         }
-
-
     }
 
-
-    private static void FirstPart(TextReader stream, bool debug)
+    private static void FirstPart(Func<TextReader> stream)
     {
-        Dictionary<string, Node> nodes;
-        nodes = LoadFile(stream, out string start);
+        Dictionary<string, Node> nodes = LoadFile(stream());
         var startNode = nodes["AA"];
         var maxTime = 30;
         var cache = new Dictionary<string, int>();
+        var productiveNodesCount = nodes.Values.Count(x => x.Flow > 0);
 
-        var result = MaxFlow(startNode, new List<string>(), maxTime);
+        var result = MaxFlow(startNode, 0ul, maxTime);
         Debug.WriteLine("result=" + result);
 
-
-
-        int MaxFlow(Node current, List<string> opened, int timeLeft)
+        int MaxFlow(Node current, ulong openedBm, int timeLeft)
         {
+            if (BitCounter.CountSetBits(openedBm) == productiveNodesCount)
+                return 0;
+
             if (timeLeft <= 0) return 0;
 
-            var key = $"{current.Name};{string.Join(',', opened)};{((char)timeLeft)}";
+            var key = $"{current.Name};{openedBm};{((char)timeLeft)}";
             if (cache.ContainsKey(key))
                 return cache[key];
 
 
             var maxFlow = 0;
-            if (current.Flow > 0 && !opened.Contains(current.Name))
+            if (current.Flow > 0 && ((openedBm & (1u << current.Number)) == 0))
             {
-                var currentOpened = opened.Append(current.Name).OrderBy(x => x).ToList();
-
-                maxFlow = (current.Flow * (timeLeft - 1)) + MaxFlow(current, currentOpened, timeLeft - 1);
+                maxFlow = (current.Flow * (timeLeft - 1)) + MaxFlow(current, openedBm | 1u << current.Number, timeLeft - 1);
             }
-
             foreach (var edge in current.Edges.Values)
             {
-                maxFlow = Math.Max(maxFlow, MaxFlow(edge.To, opened, timeLeft - 1));
+                maxFlow = Math.Max(maxFlow, MaxFlow(edge.To, openedBm, timeLeft - 1));
             }
 
             cache[key] = maxFlow;
@@ -126,21 +118,20 @@ Valve JJ has flow rate=21; tunnel leads to valve II"
 
     }
 
-    private static Dictionary<string, Node> LoadFile(TextReader stream, out string first)
+    private static Dictionary<string, Node> LoadFile(TextReader stream)
     {
         // 0     1  2    3   4   5    6      7    8    9   0   1     2
         //Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 
         var nodeList = new Dictionary<string, Node>();
-        first = "";
+        var i = 0;
         while (stream.ReadLine() is { } inpLine)
         {
             var parts = inpLine.Split(" =,;".ToCharArray(), StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
             var name = parts[1];
             var flow = parts[5].ToInt()!.Value;
             var connected = parts.Skip(10).ToList();
-            nodeList[name] = new Node(name, flow, connected);
-            if (first == "") first = name;
+            nodeList[name] = new Node(name, flow, connected, i++);
 
         }
         nodeList.Values.ForEach((n, i) =>
@@ -155,16 +146,18 @@ internal class Node
 {
     private readonly List<string> _connected;
     public string Name { get; }
+    public int Number { get; set; }
     public int Flow { get; }
     public Dictionary<string, Edge> Edges { get; } = new();
     public int IsOpen { get; set; } = 0;
     public int Visited { get; set; } = 0;
 
-    public Node(string name, int flow, List<string> connected)
+    public Node(string name, int flow, List<string> connected, int number)
     {
         _connected = connected;
         Name = name;
         Flow = flow;
+        Number = number;
     }
 
     public void Connect(Dictionary<string, Node> nodelist)
@@ -196,4 +189,28 @@ internal class Edge
     }
 }
 
+public struct BitCounter
+{
+    private static byte[] BitsSetTable256 { get; } = new byte[256];
+    static BitCounter()
+    {
+        BitsSetTable256[0] = 0;
+        for (var i = 0; i < 256; i++)
+        {
+            var bi = (byte)i;
+            BitsSetTable256[i] = (byte)((bi & 1) + BitsSetTable256[i / 2]);
+        }
+    }
+
+    public static int CountSetBits(ulong bm)
+    {
+        var bytes = BitConverter.GetBytes(bm);
+        return BitsSetTable256[bytes[0]]
+               + BitsSetTable256[bytes[1]]
+               + BitsSetTable256[bytes[2]]
+               + BitsSetTable256[bytes[3]];
+
+    }
+
+}
 
